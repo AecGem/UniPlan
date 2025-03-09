724d12a5eb2989225af552ca1a23258e8c3afb1a
seanwilcox337 <sean.wilcox337@gmail.com>
2025-03-09 13:37:12 -0600

    pages flip flip

--- frontend/src/RegistrantApp.jsx
+++ frontend/src/RegistrantApp.jsx
@@ -8,38 +8,18 @@ function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-      <div className="app-container">
-        <h1 className="heading">UniPlan: A Degree Planner</h1>
-        <p className="description">
-           Welcome to UniPlan, your personal degree-planning tool. Drag and drop courses, add semesters
-           and keep track of your journey-all in one place.
-        </p>
-
-        <div className="button-container">
-        <button className="get-planning-btn" onClick={() => setShowModal(true)}>
-          Get Planning!
-        </button>
+    <div id="centered-page">
+    <header id="subtitle">
+      <h1>UniPlan: Registrant's Homepage</h1>
+    </header>
+      <div id ="centered-page">
+        <div className="app-container">
       </div>
+      
 
-        {showModal && (
-        <div className="modal-backdrop">
-          <div className="modal-content">
-            <h2>Log In</h2>
-            <div className="login-form">
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button>Log In</button>
-              <p>
-                Don't have an account? <a href="#">Sign Up</a>
-              </p>
-            </div>
-            <button className="close-button" onClick={() => setShowModal(false)}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+      
+      </div>
+  </div>
   );
 }
 
--- frontend/src/RegistrarApp.jsx
+++ frontend/src/RegistrarApp.jsx
@@ -7,40 +7,24 @@ function App() {
   //const [count, setCount] = useState(0)
   const [showModal, setShowModal] = useState(false);
 
+
+
   return (
-      <div className="app-container">
-        <h1 className="heading">UniPlan: A Degree Planner</h1>
-        <p className="description">
-           Welcome to UniPlan, your personal degree-planning tool. Drag and drop courses, add semesters
-           and keep track of your journey-all in one place.
-        </p>
-
-        <div className="button-container">
-        <button className="get-planning-btn" onClick={() => setShowModal(true)}>
-          Get Planning!
-        </button>
-      </div>
-
-        {showModal && (
-        <div className="modal-backdrop">
-          <div className="modal-content">
-            <h2>Log In</h2>
-            <div className="login-form">
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button>Log In</button>
-              <p>
-                Don't have an account? <a href="#">Sign Up</a>
-              </p>
-            </div>
-            <button className="close-button" onClick={() => setShowModal(false)}>
-              Close
-            </button>
-          </div>
+    <div id="centered-page">
+      <header id="subtitle">
+        <h1>UniPlan: A Degree Planner</h1>
+      </header>
+        <div id ="centered-page">
+          <div className="app-container">
         </div>
-      )}
+        
+
+        
+        </div>
     </div>
   );
+
+  
 }
 
 export default App;
