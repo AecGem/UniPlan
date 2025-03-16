2e4cfe6fbbe2db717d37d25263789a6f3ee7ca75
AecGem <alex15hall156@gmail.com>
2025-03-16 14:23:41 -0600

    Changed vite config, also added a couple TODO scripts for API's.

---
+++ backend/src/middleware/RegistrationAnalyzer.ts
@@ -0,0 +1,8 @@
+//Library for running C++ program that analyzes planned registrations
+
+import {$} from "bun";
+
+/*  Overview: C++ program exists to analyze the registraton statistics of plans made by students,
+    and generates course section recommendations accordingly.
+    Program should read a big JSON document, and return another JSON document.
+*/
