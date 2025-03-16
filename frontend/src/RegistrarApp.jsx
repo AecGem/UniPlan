ac96d0b59f17b35e888c2617986cf2f00a6b537f
Cytrouillle <kaepatton14@gmail.com>
2025-03-16 23:45:25 +0000

    we cssing

--- frontend/src/RegistrarApp.jsx
+++ frontend/src/RegistrarApp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './RegistrarApp.css'
+import './Registrar.css'
 //import React, { useState } from 'react';
 //import './App.css';
 
