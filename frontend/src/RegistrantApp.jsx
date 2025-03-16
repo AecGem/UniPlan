f7d32ceef3849c11ab792e2d83dc34199e7061b5
Cytrouillle <kaepatton14@gmail.com>
2025-03-16 23:21:34 +0000

    testetst

--- frontend/src/RegistrantApp.jsx
+++ frontend/src/RegistrantApp.jsx
@@ -265,20 +265,20 @@ export default function App() {
           */}
           <CollapsibleSection
             title="Specific Required Courses"
-            items={[
+            items={
               courses
-                .filter(course => course.isambig === false)
+                //.filter(course => course.isambig === false)
                 .map(course => `${course.shortname}: ${course.coursename}`)
-            ]}
+            }
             onDragStartAside={handleDragStartAside}
           />
           <CollapsibleSection
             title="Student Selection Courses"
-            items={[
+            items={
             courses
-              .filter(course => course.isambig === true)
+              //.filter(course => course.isambig === true)
               .map(course => `${course.shortname}: ${course.coursename}`)
-            ]}
+            }
           onDragStartAside={handleDragStartAside}
           />
           <CollapsibleSection
