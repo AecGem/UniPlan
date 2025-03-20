18a02aa55f16e2df32ba727af4ee78764f7b19ca
carowill <williamsc1810@gmail.com>
2025-03-20 15:16:44 -0600

    Condensing PostgreSQL documentation

--- DATA/Queries.sql
+++
@@ -1,8 +0,0 @@
--- Return degrees
-SELECT did, degree from degree;
-
--- Update user table with selected degree
-UPDATE users SET did = "DID_IN" WHERE userid = "USERID_IN";
-
--- Get degree requirements
-SELECT req from degree WHERE did = "DID_IN";
--- DATA/saved semester table query.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE saved_sem (
-sem_id SERIAL PRIMARY KEY
-save_id INT,
-CONSTRAINT fk_degree FOREIGN KEY (save_id)
-REFERENCES saved_degree(save_id)
-courses INTEGER[],
-sname VARCHAR(50) NOT NULL
-);
