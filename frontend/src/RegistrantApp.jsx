d3485fd1ee97865656ec3fc092750fda88b045ec
Ud00ne <emmajmckay13@gmail.com>
2025-03-21 12:37:07 -0600

    testing variable pass

--- frontend/src/RegistrantApp.jsx
+++ frontend/src/RegistrantApp.jsx
@@ -55,7 +55,7 @@ export default function App() {
 
   // Fetch courses from the backend when the component mounts.
   useEffect(() => {
-    fetch('/api/course')
+    fetch('/api/course/$1')
       .then(res => res.json())
       .then(data => {
         setCourses(data);
