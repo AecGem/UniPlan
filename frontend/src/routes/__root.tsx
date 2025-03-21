702edaa8466cf5db6a6c62c40cc6428bf2b2001c
Cytrouille <kaepatton14@gmail.com>
2025-03-20 20:17:33 -0600

    We linkin?

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,25 @@
-import * as React from 'react'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import React from 'react'
+import { Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRoute } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <nav className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+        <Link to="/registrant" className="[&.active]:font-bold">
+          Registrant
+        </Link>
+        <Link to="/registrar" className="[&.active]:font-bold">
+          Registrar
+        </Link>
+      </nav>
+      <hr />
       <Outlet />
+      <TanStackRouterDevtools />
     </>
   ),
 })
