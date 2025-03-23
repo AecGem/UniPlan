50e0a20916023a6d9a082ccf258ee3606ac73f62
AecGem <alex15hall156@gmail.com>
2025-03-23 17:01:32 -0600

    Added forced-unsecure flags to curl, removed unnecessary scripts.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,8 +46,8 @@ const app = new Elysia()
             //its like 199 degrees
             let directory = '/var/www/temp/UniPlan/'.concat(id);
             await $`mkdir ${directory}`;
-            await $`curl https://localhost:443/api/degree?did=${did} > ${directory}/req.json`;
-            await $`curl https://localhost:443/api/sems_with_class?id=${id} >${directory}/sem.json` //TODO: Get the saved sem api.
+            await $`curl https://localhost:443/api/degree?did=${did} -k > ${directory}/req.json`;
+            await $`curl https://localhost:443/api/sems_with_class?id=${id} -k >${directory}/sem.json` //TODO: Get the saved sem api.
 
             //when you're doin it with me, doin it with me~!
             await $`/var/www/UniPlan/backend/middleware/build/verifier ${directory}/req.json ${directory}/sem.json ${directory}/out.json`
--- backend/src/middleware/RegistrationAnalyzer.ts
+++
@@ -1,8 +0,0 @@
-//Library for running C++ program that analyzes planned registrations
-
-import {$} from "bun";
-
-/*  Overview: C++ program exists to analyze the registraton statistics of plans made by students,
-    and generates course section recommendations accordingly.
-    Program should read a big JSON document, and return another JSON document.
-*/
