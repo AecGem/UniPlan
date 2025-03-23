5fd3648b852f50cdcbc80cf55253790dbe1a2178
seanwilcox337 <sean.wilcox337@gmail.com>
2025-03-22 18:54:17 -0600

    nothing wants to be defined I hate variables

--- frontend/src/RegistrarApp.jsx
+++ frontend/src/RegistrarApp.jsx
@@ -126,12 +126,14 @@ export const App = () => {
       }
     
 //handling functions for the dropdowns
-    //const [degrees, setDegrees] = useState(["0"]);
+    const [degrees, setDegrees] = useState(["0"]);
     const [selectedDegreeId, setSelectedDegreeId] = useState(null);
+    /*
     const setDegrees = (degreeValue) => 
       {
         setSelectedDegreeID(degreeValue);  // Now we actually have `degree` state 
       }
+        */
 
     const [degInfo, setDegInfo] = useState("0a"); // or "" if you prefer
     const handleInfoChange = (degInfoValue) => 
