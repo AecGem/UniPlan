197908b26147c4107c291c7ead67ef8529e3ced0
AecGem <alex15hall156@gmail.com>
2025-03-23 15:37:36 -0600

    contextualizing...

--- frontend/src/routes/registrar.tsx
+++ frontend/src/routes/registrar.tsx
@@ -4,6 +4,24 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/registrar')({
   component: RouteComponent,
+    //component: RootComponent,
+    beforeLoad: async ({ context, location }) => {
+      // if our ctx session isnt valid
+      if (!context.session.check()) {
+          // lets verify by checking against the api
+          var sess = await context.authClient.getSession()
+          console.log(sess);
+          // if that's really false, we need to log back in.
+          // navigating anywhere on the profile pages will force a redirect to login.
+          if(sess.data === null) {
+              return;
+          }
+          // otherwise, update our ctx so we have the session properly set
+          context.session.set(sess.data.user, sess.data.session);
+      }
+      // return a user object (idk why. i just do)
+      return { user: context.session?.user || null }
+  },
 })
 
 function RouteComponent() {
